5d8b123f6a0682e050b08f979f4e78f7ac2ff261
Jay Menorca <zai_menorca@yahoo.com>
2024-08-13 18:33:01 -0400

    generated react components, configured routing in App.js

---
+++ clinicals-app/src/components/AddClinicals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './../logo.svg';
+import './../App.css';
+
+function AddClinicals() {
+  return (
+    <b>Add Clinical</b>
+  );
+}
+
+export default AddClinicals;
---
+++ clinicals-app/src/components/Home.js
@@ -0,0 +1,10 @@
+import logo from './../logo.svg';
+import './../App.css';
+
+function Home() {
+  return (
+    <b>Home</b>
+  );
+}
+
+export default Home;
